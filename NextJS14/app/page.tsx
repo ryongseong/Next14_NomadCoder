1571ba9975fe556c495cbb7ac58de67e21c18556
ryongseong <ryongseong.dev@gmail.com>
2025-06-08 23:03:23 +0900

    2.5 'use client'

---
+++ NextJS14/app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function App() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Welcome to Our Website</h1>
+    </div>
+  );
+}
