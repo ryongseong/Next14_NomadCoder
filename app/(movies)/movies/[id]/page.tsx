684070ac28315cc28a340b01c159be8c6e2c02e7
ryongseong <ryongseong.dev@gmail.com>
2025-06-10 11:23:03 +0900

    API_RL from page -> constants

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,20 +2,23 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: {
-    id: string;
-  };
-}
-
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const { params } = props;
+  const { id } = await params;
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
+export default async function MovieDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
