55d805901c42b6ffacbf78a91b35ca19ac6c3609
ryongseong <ryongseong.dev@gmail.com>
2025-06-10 11:11:56 +0900

    4.4 Dynamic Metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
   title: "Home",
@@ -7,8 +9,6 @@ export const metadata = {
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
@@ -17,11 +17,14 @@ async function getMovies() {
 export default async function Page() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
